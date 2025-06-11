53b55e17909bd6b48096437b240827329df35793
andriyky <akrechovetskiy@gmail.com>
2025-06-11 19:07:57 +0300

    fix: 'F401' Ruff error

--- database/__init__.py
+++ database/__init__.py
@@ -1,3 +1,5 @@
 from .client import MongoClientSingleton
 from .models import Statistics, VacancyItem
 from .templates import DatabaseStatistics, DatabaseVacancies
+
+__all__ = ["DatabaseStatistics", "DatabaseVacancies", "MongoClientSingleton", "Statistics", "VacancyItem"]
