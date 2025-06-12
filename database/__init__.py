effab41f2ef3bfe1a73975e2ddee0bad96e78a7c
andriyky <akrechovetskiy@gmail.com>
2025-06-12 14:26:42 +0300

    refactor: add/remove fields
    Added: address, title, url.
    Removed: company_type.
    Moved views and applications to InteractionStats.

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,12 @@
 from .client import MongoClientSingleton
-from .models import Statistics, VacancyItem
+from .models import InteractionStats, Statistics, VacancyItem
 from .templates import DatabaseStatistics, DatabaseVacancies
 
-__all__ = ["DatabaseStatistics", "DatabaseVacancies", "MongoClientSingleton", "Statistics", "VacancyItem"]
+__all__ = [
+    "DatabaseStatistics",
+    "DatabaseVacancies",
+    "InteractionStats",
+    "MongoClientSingleton",
+    "Statistics",
+    "VacancyItem",
+]
