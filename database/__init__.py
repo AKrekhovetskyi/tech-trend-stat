e08bd8aeb8005bb12652fde5f09f035b5c0f2427
andriyky <akrechovetskiy@gmail.com>
2025-06-12 19:29:20 +0300

    refactor: 'templates' -> 'collections'

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,6 @@
 from .client import MongoClient
+from .collections import CollectionStatistics, CollectionVacancies
 from .models import InteractionStats, Statistics, VacancyItem
-from .templates import CollectionStatistics, CollectionVacancies
 
 __all__ = [
     "CollectionStatistics",
