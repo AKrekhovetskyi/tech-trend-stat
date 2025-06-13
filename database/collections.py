61d9c86d1371f7bb13e388ac5f17cc4bf11b3c02
andriyky <akrechovetskiy@gmail.com>
2025-06-13 18:46:16 +0300

    refactor: remove 'database_name'; 'fetch_vacancies'

--- database/collections.py
+++ database/collections.py
@@ -1,4 +1,4 @@
-from datetime import UTC, datetime, timedelta
+from datetime import datetime
 from typing import Any, ClassVar
 
 from pymongo import ASCENDING, DESCENDING
@@ -8,7 +8,6 @@ from .client import MongoClient
 
 class CollectionVacancies(MongoClient):
     collection_name = "vacancies"
-    database_name = "vacancy_statistics"
     indexes: ClassVar[list[tuple[str, int]]] = [
         ("category", ASCENDING),
         ("publication_date", DESCENDING),
@@ -16,16 +15,17 @@ class CollectionVacancies(MongoClient):
     ]
 
     def fetch_vacancies(
-        self,
-        category: str,
-        from_datetime: timedelta,
-        to_datetime: timedelta,
+        self, category: str, start_from_publication_date: datetime, end_date_of_publication: datetime
     ) -> list[dict[str, Any]]:
-        now = datetime.now(UTC)
         with self.collection.aggregate(
             [
                 {"$addFields": {"pubDate": {"$dateFromString": {"dateString": "$publication_date"}}}},
-                {"$match": {"category": category, "pubDate": {"$gte": now - from_datetime, "$lte": now - to_datetime}}},
+                {
+                    "$match": {
+                        "category": category,
+                        "pubDate": {"$gte": start_from_publication_date, "$lte": end_date_of_publication},
+                    }
+                },
                 {"$project": {"pubDate": 0}},
             ]
         ) as vacancies:
@@ -34,5 +34,4 @@ class CollectionVacancies(MongoClient):
 
 class CollectionStatistics(MongoClient):
     collection_name = "statistics"
-    database_name = "vacancy_statistics"
     indexes: ClassVar[list[tuple[str, int]]] = [("category", ASCENDING), ("upsert_datetime", DESCENDING)]
