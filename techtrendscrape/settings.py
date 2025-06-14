fcf6b20f75ffeb21f66d8915958a9b687757aeff
andriyky <akrechovetskiy@gmail.com>
2025-06-14 16:18:57 +0300

    refactor: uncomment 'AUTOTHROTTLE*'; add 'PROXY_LIST'

--- techtrendscrape/settings.py
+++ techtrendscrape/settings.py
@@ -9,6 +9,7 @@
 
 from datetime import timedelta
 from os import environ, getenv
+from pathlib import Path
 
 BOT_NAME = "techtrendscrape"
 
@@ -72,14 +73,14 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 2
 # The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 5
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 # AUTOTHROTTLE_DEBUG = False
 
@@ -104,4 +105,5 @@ MONGODB_CLUSTER_HOST = getenv("MONGODB_CLUSTER_HOST") or None
 if not MONGODB_HOST and not MONGODB_PORT and not MONGODB_USERNAME and not MONGODB_PASSWORD and not MONGODB_CLUSTER_HOST:
     ITEM_PIPELINES = {"techtrendscrape.pipelines.CSVPipeline": 1}
 
-PROXY_URL = getenv("PROXY_URL")
+PATH_TO_FILE_WITH_PROXIES = getenv("PATH_TO_FILE_WITH_PROXIES")
+PROXY_LIST = Path(PATH_TO_FILE_WITH_PROXIES).read_text().split() if PATH_TO_FILE_WITH_PROXIES else None
