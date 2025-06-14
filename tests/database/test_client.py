1300a1c7217324534da3d86da0201c4c879b57b8
andriyky <akrechovetskiy@gmail.com>
2025-06-14 13:48:42 +0300

    test: add 'url' field check

--- tests/database/test_client.py
+++ tests/database/test_client.py
@@ -6,11 +6,14 @@ from database import MongoClient, VacancyItem
 
 class TestMongoClient:
     def test_bulk_upsert(self, vacancy_items: list[VacancyItem], faker: Faker) -> None:
+        # Use `url` field as it's unique and all documents will be upserted.
+        url_field = "url"
+        assert url_field in VacancyItem.model_fields
+
         mongo_client = MongoClient
         mongo_client.collection_name = faker.pystr()
-        mongo_client.indexes = [("url", ASCENDING)]
+        mongo_client.indexes = [(url_field, ASCENDING)]
         with mongo_client() as client:
-            # Use `url` field as it's unique and all documents will be upserted.
             session = client.start_session()
             client.bulk_upsert(
                 ("url",),
